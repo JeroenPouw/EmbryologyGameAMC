ddc883479f35ff0b09207111abb8e72e45c17312
PepijnLinden <pepijnvanderlinden@hotmail.com>
2015-09-24 12:39:18 +0200

    Highlight GUI addition
    Text box bij highlightscript

--- Project/Assets/Scripts/HighlightScript.cs
+++ Project/Assets/Scripts/HighlightScript.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class HighlightScript : MonoBehaviour {
-	
+
+	public bool ShowThisGUI = false;
+
 	// Use this for initialization
 	void Start () {
 
@@ -10,6 +12,9 @@ public class HighlightScript : MonoBehaviour {
 
 	void OnMouseOver()
 	{
+		if (Input.GetKeyDown(KeyCode.Space)&& gameObject.layer == 4){
+			ShowThisGUI = true;
+		}
 		if (gameObject.tag != ("nonTrans") && gameObject.tag != ("Player")){
 			if (GetComponent<Renderer>().material.color.a <= 0.4f && GetComponent<Renderer>().material.shader == Shader.Find("Transparent/Diffuse")) {
 				GetComponent<Renderer> ().material.shader = Shader.Find ("Transparent/Diffuse");
@@ -20,6 +25,7 @@ public class HighlightScript : MonoBehaviour {
 
 	void OnMouseExit()
 	{
+		ShowThisGUI = false;
 		if (gameObject.tag != ("nonTrans") && gameObject.tag != ("Player")) {
 			if (GetComponent<Renderer>().material.color.a >= 0.5f && GetComponent<Renderer>().material.shader == Shader.Find("Transparent/Diffuse")) {
 				GetComponent<Renderer> ().material.shader = Shader.Find ("Transparent/Diffuse");
@@ -27,6 +33,21 @@ public class HighlightScript : MonoBehaviour {
 			}
 		}
 	}
+
+	void OnGUI() {
+		
+		if (ShowThisGUI) {
+			
+			GUI.Box(new Rect(Screen.width / 2 - 190, Screen.height / 2 - 140, 240, 320), "This is a title");
+
+			// Make the first button
+			if(GUI.Button(new Rect(Screen.width / 2 - 190, Screen.height / 2 - 140, 240, 320), "Infos")) {
+				//text
+			}
+		}
+		
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
