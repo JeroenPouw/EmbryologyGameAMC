8f27029c8bb73d39f8ce7aad9df9f0f5a5518574
Samantha Vosh <anthetem@hotmail.com>
2015-11-30 14:34:30 +0100

    Completed SaveState.cs
    All saved data can be accessed using SaveState.loaded_data
    Start of EncyTextReader

---
+++ Project/Assets/EncyTextReader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EncyTextReader : MonoBehaviour {
+
+	// Use this for initialization
+//	var savestate : SaveState;
+	//public SaveState save;
+
+	void Start () {
+		GetSaveState ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GetSaveState () {
+
+		Debug.Log(SaveState.loaded_data.puztrack + " Got Save");
+	}
+}
