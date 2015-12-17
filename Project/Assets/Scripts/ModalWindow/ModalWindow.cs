f3463f42bf93e5e03d31cd3a8df4d533a5e1a4f9
Jeroen Pouw <pouwje@gmail.com>
2015-12-17 13:40:54 +0100

    Commit 17/12/2015
    Modal window edited for Puzzle pieces
    
    ModelScene added for NMTrix

--- Project/Assets/Scripts/ModalWindow/ModalWindow.cs
+++ Project/Assets/Scripts/ModalWindow/ModalWindow.cs
@@ -8,9 +8,17 @@ public class ModalWindow : MonoBehaviour {
 	public Text message;
 	public Image iconImage;
 	public Button confirmButton;
+	public Text titletext;
 	public GameObject modalPanel;
+	public InputField input;
+	string answer;
 
 	private static ModalWindow modalWindow;
+	PuzzlePickup pickUp;
+
+	void Awake(){
+		pickUp = GetComponent<PuzzlePickup> ();
+	}
 	
 	public static ModalWindow Instance(){
 		if (!modalWindow) {
@@ -22,19 +30,50 @@ public class ModalWindow : MonoBehaviour {
 		return modalWindow;
 	}
 
-	public void Confirm(string message, UnityAction confirmAction){
+	// This function gets called when the player triggers a story trigger. Activates all the relevant UI elements.
+	public void Confirm(string message,string title, UnityAction confirmAction){
 		modalPanel.SetActive (true);
 		confirmButton.onClick.RemoveAllListeners();
 		confirmButton.onClick.AddListener (confirmAction);
 		confirmButton.onClick.AddListener (ClosePanel);
 
+	
 		this.message.text = message;
 		this.iconImage.gameObject.SetActive (false);
+		this.titletext.text = title;
 		confirmButton.gameObject.SetActive (true);
+
 	}
 
+	// This functions gets called when the player triggers a puzzle piece. Activates an inputfield where Confirm() activates a confirm button.
+	public void Question(string message, string title, UnityAction questionAction){
+		modalPanel.SetActive (true);
+		confirmButton.onClick.RemoveAllListeners();
+		confirmButton.onClick.AddListener (questionAction);
+		confirmButton.onClick.AddListener (ClosePanel);
+
+		this.answer = PuzzlePickup.answer;
+		this.message.text = message;
+		this.titletext.text = title;
+		input.gameObject.SetActive (true);
+	}
+
+	// This functions gets called when the player presses the 'Okay' button.
 	void ClosePanel(){
 		modalPanel.SetActive (false);
+		if (Time.timeScale != 1) {
+			Time.timeScale = 1;
+		}
+	}
+
+	// This function gets called when the player finishes typing, or confirms their input.
+	public void InputCheck(){
+		Debug.Log (input.text);
+		Debug.Log (this.answer);
+		if (input.text == this.answer) {
+			input.gameObject.SetActive(false);
+			confirmButton.gameObject.SetActive(true);
+		}
 	}
 
 
