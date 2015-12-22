59d667c0ef29281197fe32450efe0ba9f1ec4b59
Samantha Vosh <anthetem@hotmail.com>
2015-12-22 16:23:42 +0100

    Foundation of Compass code completed. Possibility to create waypoints using the compass. Texture needs to be made for it.

---
+++ Project/Assets/Scripts/ShipUI/Compass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compass : MonoBehaviour {
+
+	Transform player;
+	public bool stage = true;
+	public Transform anchor8;
+	public Transform anchor10;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stage) {
+			this.transform.rotation = Quaternion.LookRotation ((Vector3.zero - player.position), Vector3.up);
+		} else {
+			this.transform.rotation = Quaternion.LookRotation ((Vector3.zero - player.position), Vector3.up);
+		}
+	}
+}
