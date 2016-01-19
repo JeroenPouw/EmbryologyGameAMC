79ef9cd0b84fcb234020098ea6144363810c8854
luperizer <yoobin.kim88@gmail.com>
2016-01-19 16:21:07 +0100

    Battery + Rotation

--- Project/Assets/Scripts/ItemRotation.cs
+++ Project/Assets/Scripts/ItemRotation.cs
@@ -5,12 +5,12 @@ public class ItemRotation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		transform.rotation = Random.rotation;
+		//transform.rotation = Random.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.forward * Time.deltaTime * 50, Space.Self);
+		transform.Rotate(Vector3.forward * Time.deltaTime * 50);
 		transform.Rotate(Vector3.up * Time.deltaTime * 75, Space.World);
 	}
 }
