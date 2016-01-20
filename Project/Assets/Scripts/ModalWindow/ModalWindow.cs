1a061634683e975aa4fc3ef83ef288352dff8a5c
Jeroen Pouw <pouwje@gmail.com>
2016-01-20 11:53:05 +0100

    Commit 20-01-2016
    fixed crash on camera switch while modalwindow was on.

--- Project/Assets/Scripts/ModalWindow/ModalWindow.cs
+++ Project/Assets/Scripts/ModalWindow/ModalWindow.cs
@@ -12,12 +12,14 @@ public class ModalWindow : MonoBehaviour {
 	public GameObject modalPanel;
 	public InputField input;
 	string answer;
+	GameObject player;
 
 	private static ModalWindow modalWindow;
 	PuzzlePickup pickUp;
 
 	void Awake(){
 		pickUp = GetComponent<PuzzlePickup> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	public static ModalWindow Instance(){
@@ -37,6 +39,7 @@ public class ModalWindow : MonoBehaviour {
 		confirmButton.onClick.RemoveAllListeners();
 		confirmButton.onClick.AddListener (confirmAction);
 		confirmButton.onClick.AddListener (ClosePanel);
+		player.GetComponent<CameraSwitch> ().enabled = false;
 
 	
 		this.message.text = message;
@@ -53,6 +56,7 @@ public class ModalWindow : MonoBehaviour {
 		confirmButton.onClick.RemoveAllListeners();
 		confirmButton.onClick.AddListener (questionAction);
 		confirmButton.onClick.AddListener (ClosePanel);
+		//player.GetComponent<CameraSwitch> ().enabled = false;
 
 		this.answer = PuzzlePickup.answer;
 		this.message.text = message;
@@ -62,7 +66,7 @@ public class ModalWindow : MonoBehaviour {
 
 	// This functions gets called when the player presses the 'Okay' button.
 	void ClosePanel(){
-		Time.timeScale = 1;
+		player.GetComponent<CameraSwitch> ().enabled = true;
 		confirmButton.gameObject.SetActive (false);
 		modalPanel.SetActive (false);
 		if (Time.timeScale != 1) {
@@ -77,6 +81,12 @@ public class ModalWindow : MonoBehaviour {
 		if (input.text == this.answer) {
 			input.gameObject.SetActive(false);
 			confirmButton.gameObject.SetActive(true);
+		}
+	}
+
+	void Update(){
+	if (player == null) {
+			player = GameObject.FindGameObjectWithTag ("Player");
 		}
 	}
 
