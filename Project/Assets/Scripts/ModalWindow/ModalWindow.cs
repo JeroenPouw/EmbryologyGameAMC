efe9fd83fd0da2677c9cc56f5f7d62ae42f72d4e
Samantha Vosh <anthetem@hotmail.com>
2016-01-26 16:26:03 +0100

    Reset Overworld cs 08.

--- Project/Assets/Scripts/ModalWindow/ModalWindow.cs
+++ Project/Assets/Scripts/ModalWindow/ModalWindow.cs
@@ -12,14 +12,12 @@ public class ModalWindow : MonoBehaviour {
 	public GameObject modalPanel;
 	public InputField input;
 	string answer;
-	GameObject player;
 
 	private static ModalWindow modalWindow;
 	PuzzlePickup pickUp;
 
 	void Awake(){
 		pickUp = GetComponent<PuzzlePickup> ();
-		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	public static ModalWindow Instance(){
@@ -39,7 +37,6 @@ public class ModalWindow : MonoBehaviour {
 		confirmButton.onClick.RemoveAllListeners();
 		confirmButton.onClick.AddListener (confirmAction);
 		confirmButton.onClick.AddListener (ClosePanel);
-		player.GetComponent<CameraSwitch> ().enabled = false;
 
 	
 		this.message.text = message;
@@ -56,7 +53,6 @@ public class ModalWindow : MonoBehaviour {
 		confirmButton.onClick.RemoveAllListeners();
 		confirmButton.onClick.AddListener (questionAction);
 		confirmButton.onClick.AddListener (ClosePanel);
-		//player.GetComponent<CameraSwitch> ().enabled = false;
 
 		this.answer = PuzzlePickup.answer;
 		this.message.text = message;
@@ -66,7 +62,7 @@ public class ModalWindow : MonoBehaviour {
 
 	// This functions gets called when the player presses the 'Okay' button.
 	void ClosePanel(){
-		player.GetComponent<CameraSwitch> ().enabled = true;
+		Time.timeScale = 1;
 		confirmButton.gameObject.SetActive (false);
 		modalPanel.SetActive (false);
 		if (Time.timeScale != 1) {
@@ -81,12 +77,6 @@ public class ModalWindow : MonoBehaviour {
 		if (input.text == this.answer) {
 			input.gameObject.SetActive(false);
 			confirmButton.gameObject.SetActive(true);
-		}
-	}
-
-	void Update(){
-	if (player == null) {
-			player = GameObject.FindGameObjectWithTag ("Player");
 		}
 	}
 
