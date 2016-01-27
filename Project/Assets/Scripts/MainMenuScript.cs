1af5083af983c918b60432d625139e4c32f7bb6e
Samantha Vosh <anthetem@hotmail.com>
2016-01-27 15:40:26 +0100

    Main menu integrated with SaveState
    Conflicts:
    	Project/Assets/Scenes/Menus/StartScreen.unity
    	Project/Assets/Scenes/StartScreen.unity
    	Project/Assets/Scripts/MainMenuScript.cs
    	Project/Assets/Scripts/SaveState.cs

--- Project/Assets/Scripts/MainMenuScript.cs
+++ Project/Assets/Scripts/MainMenuScript.cs
@@ -3,8 +3,27 @@ using System.Collections;
 
 public class MainMenuScript : MonoBehaviour {
 
+	private SaveState save;
+
+	void Start () {
+		save = GameObject.Find ("SaveState").GetComponent<SaveState> ();
+		if (save.loaded_data.lvl == 0) {
+			GameObject.Find("ContinueButton").SetActive(false);
+		}
+	}
+
+	public void ContinueGame () {
+		if (save.loaded_data.lvl != 0) {
+
+		}
+	}
+
 	public void LevelSwitch(int sceneToChangeTo){
 		Application.LoadLevel(sceneToChangeTo);
+	}
+
+	public void CloseGame(){
+		Application.Quit ();
 	}
 
 }
--- Project/Assets/Scripts/SaveState.cs
+++ Project/Assets/Scripts/SaveState.cs
@@ -32,48 +32,44 @@
 //		catch (FileNotFoundException _exc)
 //		{
 //			loaded_data = new SaveData{
-//				mistrack = "1o2o3o4o5o6o",
-//				puztrack = "1o2o3o4o5o6o7o8o9o10o11o12o13o14o15o"
-//				storytrigger = "1";
-//			};
-//			WriteFile();
-//		}
-//	}
-//
-//	public void SaveVariable(int _missiontracker, int _puzzletracker, string _storytrigger)	{
-//		string _x = loaded_data.mistrack;
-//		string _y = loaded_data.puztrack;
-//		string _s = loaded_data.storytrigger;
-//		if (_missiontracker != 0 && _missiontracker <= 6) {
-//			if (loaded_data.mistrack.Contains (_missiontracker.ToString () + "o")) {
-//				_x = loaded_data.mistrack.Replace (_missiontracker.ToString () + "o", _missiontracker.ToString () + "x");
-//			}
-//		} else //this prevents a double save for the reduction of 2 calcuations. Worth it?
-//		if (_puzzletracker != 0 && _puzzletracker <= 15) {
-//			if (loaded_data.puztrack.Contains (_puzzletracker.ToString () + "o")) {
-//				_y = loaded_data.puztrack.Replace (_puzzletracker.ToString () + "o", _puzzletracker.ToString () + "x");
-//			}
-//		} else
-//		if (_storytrigger != "") {
-//			_s = _storytrigger;
-//		}
-//
-//		loaded_data = new SaveData{
-//			mistrack = _x,
-//			puztrack = _y,
-//			storytrigger = _s
-//		};
-//		WriteFile (); //maybe decided to not put this here.
-//	}
-//
-//	[Serializable]
-//	public struct SaveData //remember, structs can't change data, a change only copies the struct
-//	{
-//		public string mistrack;
-//		public string puztrack;
-//		public string storytrigger;
-//	}
-//	
-//}
-//
-//
+				lvl = 0,
+				puztrack = "1o2o3o4o5o6o7o8o9o10o11o12o13o14o15o",
+				storytrigger = "1"
+			};
+			WriteFile();
+		}
+	}
+
+	public void SaveVariable(int _missiontracker, int _puzzletracker, string _storytrigger)	{
+		int _x = loaded_data.lvl;
+		string _y = loaded_data.puztrack;
+		string _s = loaded_data.storytrigger;
+		if (_missiontracker != 0) {
+			_x = _missiontracker;
+		} else //this prevents a double save for the reduction of 2 calcuations. Worth it?
+		if (_puzzletracker != 0 && _puzzletracker <= 15) {
+			if (loaded_data.puztrack.Contains (_puzzletracker.ToString () + "o")) {
+				_y = loaded_data.puztrack.Replace (_puzzletracker.ToString () + "o", _puzzletracker.ToString () + "x");
+			}
+		} else
+		if (_storytrigger != "") {
+			_s = _storytrigger;
+		}
+
+		loaded_data = new SaveData{
+			lvl = _x,
+			puztrack = _y,
+			storytrigger = _s
+		};
+		WriteFile (); //maybe decided to not put this here.
+	}
+
+	[Serializable]
+	public struct SaveData //remember, structs can't change data, a change only copies the struct
+	{
+		public int lvl;
+		public string puztrack;
+		public string storytrigger;
+	}
+}
+
